b2d324a7fb5d5085d9de607f840e5b9e2d09b83a
yogskr <yogakrisanta@gmail.com>
2025-01-12 15:12:44 +0700

    Deleted comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
-// Import components
 import MobileNavBar from "./components/mobile-nav/MobileNavBar";
 
 // Import page components
