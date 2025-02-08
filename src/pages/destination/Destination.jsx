57cce3bcc89740995f6a54cff823af1142c2ac5b
yogskr <yogakrisanta@gmail.com>
2025-02-08 20:47:46 +0700

    Refactor destination page layout and update styling for improved structure and content presentation

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -3,7 +3,40 @@ import "./module.Destination.css";
 export default function Destination() {
   return (
     <>
-      <div className="destination">Destination</div>
+      <div className="destinationContainer">
+        <div className="destinationContent">
+          <div className="pageTitle">
+            <h2 className="titleNumber">01</h2>
+            <h2 className="titleText">PICK YOUR DESTINATION</h2>
+          </div>
+          <div className="destinationImage"></div>
+          <div className="destinationExplanation">
+            <nav className="explanationNavigation">
+              <ul>
+                <li>
+                  <p>MOON</p>
+                </li>
+                <li>
+                  <p>MARS</p>
+                </li>
+                <li>
+                  <p>EUROPA</p>
+                </li>
+                <li>
+                  <p>TITAN</p>
+                </li>
+              </ul>
+            </nav>
+            <h3 className="explanationTitle">MOON</h3>
+            <p className="explanationText">
+              See our planet as you’ve never seen it before. A perfect relaxing
+              trip away to help regain perspective and come back refreshed.
+              While you’re there, take in some history by visiting the Luna 2
+              and Apollo 11 landing sites.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
