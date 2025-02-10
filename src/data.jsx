8fa7d7956eb8bb22b7bd0f1423047b8600569205
yogskr <yogakrisanta@gmail.com>
2025-02-10 16:40:32 +0700

    Add navigation titles and update image paths for destination planets

--- src/data.jsx
+++ src/data.jsx
@@ -32,38 +32,42 @@ export const links = [
 export const destinationPlanets = [
   {
     id: 1,
+    navTitle: "MOON",
     title: "MOON",
     description:
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
     distance: "384,400 km",
     travelTime: "3 days",
-    image: "moon",
+    image: "/assets/destination/image-moon.png",
   },
   {
     id: 2,
+    navTitle: "MARS",
     title: "MARS",
     description:
       "Experience the red planet up close and personal. Take a tour of the Olympus Mons, the largest volcano in the solar system, and the Valles Marineris, the largest canyon.",
     distance: "54.6 million km",
     travelTime: "9 months",
-    image: "mars",
+    image: "/assets/destination/image-mars.png",
   },
   {
     id: 3,
+    navTitle: "EUROPA",
     title: "EUROPA",
     description:
       "Visit Jupiter’s moon Europa, where you can ice skate on the frozen surface. Take a dip in the subsurface ocean and see the Great Lakes of Europa.",
     distance: "628.3 million km",
     travelTime: "3 years",
-    image: "europa",
+    image: "/assets/destination/image-europa.png",
   },
   {
     id: 4,
+    navTitle: "TITAN",
     title: "TITAN",
     description:
       "Titan is Saturn’s largest moon. It has a thick atmosphere and methane lakes. Take a boat tour on the Kraken Mare and see the Huygens probe.",
     distance: "1.2 billion km",
     travelTime: "7 years",
-    image: "titan",
+    image: "/assets/destination/image-titan.png",
   },
 ];
