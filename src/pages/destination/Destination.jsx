b60552db17a5137b10ac53758e1b41e76aa8c25d
yogskr <yogakrisanta@gmail.com>
2025-02-10 16:42:40 +0700

    Refactor Destination component to integrate DestinationExplanation for improved navigation and content organization

--- src/pages/destination/Destination.jsx
+++ src/pages/destination/Destination.jsx
@@ -1,42 +1,17 @@
 import "./module.Destination.css";
+import DestinationExplanation from "../../components/destination-navbar/DestinationExplanation.jsx";
 
 export default function Destination() {
   return (
-    <>
-      <div className="destinationContainer">
-        <div className="destinationContent">
-          <div className="pageTitle">
-            <h2 className="titleNumber">01</h2>
-            <h2 className="titleText">PICK YOUR DESTINATION</h2>
-          </div>
-          <div className="destinationImage"></div>
-          <div className="destinationExplanation">
-            <nav className="explanationNavigation">
-              <ul>
-                <li>
-                  <p className="tabText">MOON</p>
-                </li>
-                <li>
-                  <p className="tabText">MARS</p>
-                </li>
-                <li>
-                  <p className="tabText">EUROPA</p>
-                </li>
-                <li>
-                  <p className="tabText">TITAN</p>
-                </li>
-              </ul>
-            </nav>
-            <h3 className="explanationTitle">MOON</h3>
-            <p className="explanationText">
-              See our planet as you’ve never seen it before. A perfect relaxing
-              trip away to help regain perspective and come back refreshed.
-              While you’re there, take in some history by visiting the Luna 2
-              and Apollo 11 landing sites.
-            </p>
-          </div>
+    <div className="mainContent">
+      <div className="destinationContent">
+        <div className="pageTitle">
+          <h2 className="titleNumber">01</h2>
+          <h2 className="titleText">PICK YOUR DESTINATION</h2>
         </div>
+        {/* Destination Navigation Bar */}
+        <DestinationExplanation />
       </div>
-    </>
+    </div>
   );
 }
