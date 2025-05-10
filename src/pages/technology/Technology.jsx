be3d8fb04a92a36e3a3a7382049cfb260db77f19
yogskr <yogakrisanta@gmail.com>
2025-05-10 18:47:11 +0700

    Implement Technology page layout with title and styling

--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -1,3 +1,14 @@
+import "../technology/module.Technology.css";
+
 export default function Technology() {
-  return <div>Technology</div>;
+  return (
+    <div className="mainContent">
+      <div className="technologyContent">
+        <div className="pageTitle">
+          <h2 className="titleNumber">03</h2>
+          <h2 className="titleText">SPACE LAUNCH 101</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
