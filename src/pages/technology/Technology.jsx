d291c0f9c88c5629bbb57f382f9848b88640c5be
yogskr <yogakrisanta@gmail.com>
2025-05-15 22:44:54 +0700

    Refactor Technology component structure and update main content class name

--- src/pages/technology/Technology.jsx
+++ src/pages/technology/Technology.jsx
@@ -1,14 +1,17 @@
 import "../technology/module.Technology.css";
+import TechnologyInfo from "../../components/technology-info/TechnologyInfo";
 
 export default function Technology() {
   return (
-    <div className="mainContent">
+    <div className="mainTechContent">
       <div className="technologyContent">
         <div className="pageTitle">
           <h2 className="titleNumber">03</h2>
           <h2 className="titleText">SPACE LAUNCH 101</h2>
         </div>
       </div>
+      {/* Explanation */}
+      <TechnologyInfo />
     </div>
   );
 }
