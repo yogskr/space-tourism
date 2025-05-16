b4caa013acdb50898cbb4fa0fa8f4968fad0b63b
yogskr <yogakrisanta@gmail.com>
2025-05-17 01:49:13 +0700

    Add background images data structure and update comments

--- src/data.jsx
+++ src/data.jsx
@@ -1,3 +1,7 @@
+// Data for the Space Tourism website
+// This file contains all the data used in the application, including links, destination planets, crew members, and technology items.
+
+// Navigation Links
 export const links = [
   {
     id: 1,
@@ -29,6 +33,31 @@ export const links = [
   },
 ];
 
+// Background Images
+export const backgroundImages = {
+  home: {
+    mobile: "/assets/home/background-home-mobile.jpg",
+    tablet: "/assets/home/background-home-tablet.jpg",
+    desktop: "/assets/home/background-home-desktop.jpg",
+  },
+  destination: {
+    mobile: "/assets/destination/background-destination-mobile.jpg",
+    tablet: "/assets/destination/background-destination-tablet.jpg",
+    desktop: "/assets/destination/background-destination-desktop.jpg",
+  },
+  crew: {
+    mobile: "/assets/crew/background-crew-mobile.jpg",
+    tablet: "/assets/crew/background-crew-tablet.jpg",
+    desktop: "/assets/crew/background-crew-desktop.jpg",
+  },
+  technology: {
+    mobile: "/assets/technology/background-technology-mobile.jpg",
+    tablet: "/assets/technology/background-technology-tablet.jpg",
+    desktop: "/assets/technology/background-technology-desktop.jpg",
+  },
+};
+
+// Destination Planets
 export const destinationPlanets = [
   {
     id: 1,
@@ -72,6 +101,7 @@ export const destinationPlanets = [
   },
 ];
 
+// Crew Members
 export const crewMembers = [
   {
     id: 1,
